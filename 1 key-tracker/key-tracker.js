668c0f08452f4184db3e2c0e0d0e95cd4094b128
Tejas Khurd <tejas.khurd.work@gmail.com>
2026-03-22 15:20:45 +0530

    add key-tracker

---
+++ 1 key-tracker/key-tracker.js
@@ -0,0 +1,10 @@
+// window means screen so event listener screen pe lagaya ahi
+
+window.addEventListener("keydown", (elem) => {
+  console.log(elem);
+  if (elem.key == " ") document.querySelector("h1").innerText = `Space`;
+  else document.querySelector("h1").innerText = elem.key;
+});
+
+// keydown → Fires when key is pressed down
+// keyup → Fires when key is released
